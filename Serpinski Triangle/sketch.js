4d2887aab6fe454be9d96c47586b9b2d42bd9399
Winter091 <winter091@yandex.ru>
2019-10-24 13:45:22 +0300

    Commit

--- Serpinski Triangle/sketch.js
+++ Serpinski Triangle/sketch.js
@@ -1,41 +1,45 @@
-var N = 5;
+var N = 12;
+let slider;
 
 function setup()
 {
   createCanvas(600, 600);
+  slider = createSlider(2, 10, 5, 1);
+  slider.position(10, 10);
 }
 
 function draw()
 {
   background(0);
-  noLoop();
+  //noLoop();
   noStroke();
   fill(255);
 
   translate(width / 2, height);
   triangle(-width / 2, 0, 0, -height, width / 2, 0);
-  fillTriangle(width / 2, height / 2, 1);
+  fillTriangle(width / 2, height / 2, 1, slider.value());
   //save("SerpinskiTriangle.png");
 }
 
-function fillTriangle(w, h, iter) {
+function fillTriangle(w, h, iter, N) {
   fill(0);
   triangle(0, 0, -w / 2, -h, w / 2, -h);
 
   if (iter < N) {
     push();
     translate(-w / 2, 0);
-    fillTriangle(w / 2, h / 2, iter + 1);
+    fillTriangle(w / 2, h / 2, iter + 1, N);
     pop();
 
     push();
     translate(w / 2, 0);
-    fillTriangle(w / 2, h / 2, iter + 1);
+    fillTriangle(w / 2, h / 2, iter + 1, N);
     pop();
 
     push();
     translate(0, -h);
-    fillTriangle(w / 2, h / 2, iter + 1);
+    fillTriangle(w / 2, h / 2, iter + 1, N);
     pop();
   }
 }
+
